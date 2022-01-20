b355586570a2fb952035072de84474e47f02fa46
Satyam BHawsinghka <bhawsins@oregonstate.edu>
2022-01-19 19:54:59 -0800

    fix

--- assignments/week3/Test.py
+++ assignments/week3/Test.py
@@ -45,29 +45,48 @@ class Interpretation(Sensing):
         if self.polarity == 0:
             r1 = data[1] / data[0]
             r2 = data[1] / data[2]
+            if r1 > self.sensitivity and r2 > self.sensitivity:
+                direction = 'center'
+                degree = 0
+            elif r1 > self.sensitivity > r2:
+                direction = 'left'
+                degree = r1-r2
+            elif r2 > self.sensitivity > r1:
+                direction = 'right'
+                degree = r2-r1
+            else:
+                if data[0] > self.sensitivity * data[1] and data[0] > self.sensitivity * data[2]:
+                    direction = 'right'
+                    degree = 2 * data[0]/(data[1]+data[2])
+                if data[2] > self.sensitivity * data[1] and data[2] > self.sensitivity * data[0]:
+                    direction = 'left'
+                    degree = 2 * data[2]/(data[1]+data[0])
+                else:
+                    direction = 'same'
+                    degree = 0
 
         if self.polarity == 1:
             r1 = data[0] / data[1]
             r2 = data[2] / data[1]
-
-        if r1 > self.sensitivity and r2 > self.sensitivity:
-            direction = 'center'
-            degree = 0
-        elif r1 > self.sensitivity > r2:
-            direction = 'left'
-            degree = r1-r2
-        elif r2 > self.sensitivity > r1:
-            direction = 'right'
-            degree = r2-r1
-        elif 1 < r1 < self.sensitivity and 1 < r2 < self.sensitivity:
-            direction = 'same'
-            degree = 0
-        elif r1 < 1 < r2:
-            direction = 'right'
-            degree = 1/r1 - 1/r2
-        elif r2 < 1 < r1:
-            direction = 'left'
-            degree = 1/r2 - 1/r1
+            if r1 > self.sensitivity and r2 > self.sensitivity:
+                direction = 'center'
+                degree = 0
+            elif r1 > self.sensitivity > r2:
+                direction = 'left'
+                degree = r1-r2
+            elif r2 > self.sensitivity > r1:
+                direction = 'right'
+                degree = r2-r1
+            else:
+                if data[1] > self.sensitivity * data[0] and data[2] > self.sensitivity * data[0]:
+                    direction = 'right'
+                    degree = 2 * data[0]/(data[1]+data[2])
+                if data[1] > self.sensitivity * data[2] and data[0] > self.sensitivity * data[2]:
+                    direction = 'left'
+                    degree = 2 * data[2]/(data[1]+data[0])
+                else:
+                    direction = 'same'
+                    degree = 0
 
 
         return direction, degree
