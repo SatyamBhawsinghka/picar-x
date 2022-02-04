f0f54c2478b1315f804010d981c353880decb5a9
Satyam BHawsinghka <bhawsins@oregonstate.edu>
2022-02-04 10:40:10 -0800

    Creating ultransonic snesing, interpretation and controller classes

---
+++ assignments/week5/Ultrasonic.py
@@ -0,0 +1,71 @@
+import sys
+import atexit
+sys.path.append(r'/home/satyam/picar-x/lib')
+from picarx_improved import Picarx
+import time
+import numpy
+
+
+class Sensing(Picarx):
+    def __init__(self):
+        super().__init__()
+
+    def get_data(self):
+        return self.Get_distance()
+
+    # Returns mean over 5 data values
+    def read(self):
+        values = []
+        for i in range(5):
+            values.append(self.get_data())
+        avg = numpy.mean(values, axis=0)
+        return avg
+
+
+class Interpretation(object):
+    def __init__(self, stopping_range=1):
+        # Range in cm at which the car needs to be stopped
+        self.stopping_range = stopping_range
+
+    # Processing sensor data
+    def processing(self, distance):
+        if data < self.stopping_range:
+            return 1
+        else:
+            return 0
+
+
+ class Controller(Picarx):
+    def __init__(self):
+        super().__init__()
+
+    def control(self, go):
+        if go:
+            self.forward(30)
+
+        else:
+            self.stop()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    time.sleep(3)
+    sensor = Sensing()
+    processor = Interpretation()
+    controller = Controller()
+
+    try:
+        while True:
+            distance = sensor.read()
+            print(distance)
+            controller.control(processor.processing(distance))
+    except:
+        print("Error in execution")
+        atexit.register(controller.stop)
+
+    finally:
+        atexit.register(controller.stop)
