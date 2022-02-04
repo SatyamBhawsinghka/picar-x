3246dde88378548f5b55b07dc7e9e02a076c1d0d
Satyam BHawsinghka <bhawsins@oregonstate.edu>
2022-02-03 20:48:52 -0800

    fix

--- assignments/week4/consumer_producer.py
+++ assignments/week4/consumer_producer.py
@@ -39,9 +39,9 @@ if __name__ == "__main__":
 
     try:
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-            sense = executor.submit(producer, 0.05, sensor_bus, sensor)
-            process = executor.submit(consumer_producer, 0.05, sensor_bus, processor_bus, processor)
-            control = executor.submit(consumer, 0.05, processor_bus, controller)
+            sense = executor.submit(producer, 0.5, sensor_bus, sensor)
+            process = executor.submit(consumer_producer, 0.5, sensor_bus, processor_bus, processor)
+            control = executor.submit(consumer, 0.5, processor_bus, controller)
         # print(sense.result())
         # print(process.result())
         # print(control.result())
