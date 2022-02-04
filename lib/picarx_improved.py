1507ad228b5d5e1e8cafbbc8a680736a8325e57a
Satyam BHawsinghka <bhawsins@oregonstate.edu>
2022-02-04 11:09:33 -0800

    Changed timed out condition in ultrasonic module

--- lib/picarx_improved.py
+++ lib/picarx_improved.py
@@ -262,11 +262,13 @@ class Picarx(object):
         while echo.value() == 0:
             pulse_start = time.time()
             if pulse_start - timeout_start > timeout:
-                return -1
+                # return -1
+                pass
         while echo.value() == 1:
             pulse_end = time.time()
             if pulse_end - timeout_start > timeout:
-                return -2
+                #return -2
+                pass
         during = pulse_end - pulse_start
         cm = round(during * 340 / 2 * 100, 2)
         # print(cm)
